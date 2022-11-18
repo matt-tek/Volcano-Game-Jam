cecae848de1b3cdb7e6027f622304d47a61db83b
matt m <mathieu.muty@epitech.eu>
2022-11-19 03:12:14 +0400

    init project

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private Rigidbody2D body;
+    private bool isGrounded;
+    [SerializeField] private float speed;
+    [SerializeField] private int jumpPower;
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+        if (Input.GetButtonDown("Jump"))
+            body.velocity = new Vector2(body.velocity.x, jumpPower);
+    }
+}
