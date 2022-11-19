22939d77fe0aee4d4f6f8dedf64c6ef5f66cff78
theotrp <theo.turpin@epitech.eu>
2022-11-20 00:15:05 +0400

    Add pause Menu + song

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    public GameObject pauseMenuUI;
+    public GameObject settingsPanel;
+    public AudioSource audioSource;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paused();
+            }
+        }
+    }
+
+    public void SettingsBtn()
+    {
+        settingsPanel.SetActive(true);
+    }
+    public void CloseSettingsBtn()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    void Paused()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale =  1;
+        gameIsPaused = false;
+    }
+
+    public void MainMenu()
+    {
+        Resume();
+        SceneManager.LoadScene("Menu");
+    }
+}
